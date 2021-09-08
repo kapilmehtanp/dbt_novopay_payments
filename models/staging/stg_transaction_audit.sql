ea2cabd268d7a9a9ebbd5fe1799586d379ff8d0e
Kapil Mehta <kapil.mehta@novopay.in>
2021-09-08 16:33:55 +0530

    First Commit

--- models/staging/stg_transaction_audit.sql
+++ models/staging/stg_transaction_audit.sql
@@ -4,7 +4,7 @@ with source as (
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
     #}
-    select * from {{ ref('raw_transaction_audit') }}
+    select * from {{ ref('stg_transaction_audit') }}
 
 ),
 
