1ad80f96e31bae8c0876166860781c6e2a1c172f
Kapil Mehta <kapil.mehta@novopay.in>
2021-09-21 14:07:46 +0530

    Select function changes

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,15 @@
 
 -- Use the `ref` function to select from other models
 
-select *
+select  id,
+        transaction_type,
+        transaction_sub_type,
+        novopay_reference_code,
+        accounting_reference_code,
+        client_code,
+        client_reference_code,
+        external_reference_code,
+        partner_reference_code,
+        additional_reference_code
 from {{ ref('my_first_dbt_model') }}
 where id = 1
