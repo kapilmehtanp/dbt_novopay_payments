a3ea0e1db89b121a407b52e490d572eb94134cb8
Kapil Mehta <kapil.mehta@novopay.in>
2021-09-23 11:33:15 +0530

    Txn Audit Changes View 6

--- models/datapipeline/transaction_audit_model.sql
+++ models/datapipeline/transaction_audit_model.sql
@@ -9,7 +9,7 @@
 
 
 {{ config(
-    materialized="view",
+    materialized="table",
     schema="dk_payments"
 ) }}
 
@@ -20,12 +20,30 @@ with es_transaction_audit as (
         transaction_sub_type,
         novopay_reference_code,
         accounting_reference_code,
-        client_code,
         client_reference_code,
         external_reference_code,
         partner_reference_code,
         additional_reference_code,
-        stan
+        stan,
+        customer_identifier_type,
+        customer_identifier_value,
+        transaction_amount,
+        transaction_charges,,
+        total_amount,
+        intial_status,
+        status,
+        partner_status,
+        remarks,
+        reversal_date,
+        reversal_accounting_reference_code,
+        reversal_accounting_status,
+        partner_recon_status,
+        partner_reversal_status,
+        created_by,
+        created_on,
+        updated_by,
+        updated_on
+
 
     from servicebotdb.dk_payments.transaction_audit
 )
@@ -33,11 +51,37 @@ with es_transaction_audit as (
 
 
 select
+    es_transaction_audit.id,
     transaction_audit_additional_data.novopay_reference_code,
     transaction_audit_additional_data.label,
     transaction_audit_additional_data.value,
-    es_transaction_audit.transaction_type,
-    es_transaction_audit.transaction_sub_type
+        es_transaction_audit.transaction_type,
+        es_transaction_audit.transaction_sub_type,
+        es_transaction_audit.novopay_reference_code,
+        es_transaction_audit.accounting_reference_code,
+        es_transaction_audit.client_reference_code,
+        es_transaction_audit.external_reference_code,
+        es_transaction_audit.partner_reference_code,
+        es_transaction_audit.additional_reference_code,
+        es_transaction_audit.stan,
+        es_transaction_audit.customer_identifier_type,
+        es_transaction_audit.customer_identifier_value,
+        es_transaction_audit.transaction_amount,
+        es_transaction_audit.transaction_charges,,
+        es_transaction_audit.total_amount,
+        es_transaction_audit.intial_status,
+        es_transaction_audit.status,
+        es_transaction_audit.partner_status,
+        es_transaction_audit.remarks,
+        es_transaction_audit.reversal_date,
+        es_transaction_audit.reversal_accounting_reference_code,
+        es_transaction_audit.reversal_accounting_status,
+        es_transaction_audit.partner_recon_status,
+        es_transaction_audit.partner_reversal_status,
+        es_transaction_audit.created_by,
+        es_transaction_audit.created_on,
+        es_transaction_audit.updated_by,
+        es_transaction_audit.updated_on
 
 from servicebotdb.dk_payments.transaction_audit_additional_data
 
